8a7c9cc74c5a0dd89ac88b62412972b936655600
Lamia Islam Mim <lamiaislam2001@gmail.com>
2025-08-07 02:11:30 +0600

    Add backend folder, gave database, api,cloudinary connection

---
+++ backend/controllers/productController.js
@@ -0,0 +1,49 @@
+
+
+
+// Funtion for add products
+const addProduct = async (req, res) => {
+
+    try {
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body
+
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        console.log(name, description, price, category, subCategory, sizes, bestseller)
+        console.log(image1, image2, image3, image4)
+
+        res.json({})
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
+
+
+// Function for list products 
+const listProducts = async (req, res) => {
+
+
+}
+
+
+// Function for removing products 
+const removeProduct = async (req, res) => {
+
+
+}
+
+
+// Function for single product info 
+const singleProduct = async (req, res) => {
+
+
+}
+
+export { listProducts, addProduct, removeProduct, singleProduct }
