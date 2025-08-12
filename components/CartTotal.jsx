47af9d81ed5b79d4f00a822435b22dafc2b39d56
Syed Tashriful Alam <168919494+Tashrif67@users.noreply.github.com>
2025-08-13 04:17:38 +0600

    Done Cart management with quantity controls and did Documentatioin

--- components/CartTotal.jsx
+++ components/CartTotal.jsx
@@ -1,32 +1,46 @@
 import React from 'react'
-import {shopContext} from '../context/shopContext'
+import { shopContext } from '../context/shopContext'
 import Title from './Title'
-{/* In this componenet wiil display the total cart amouont*/}
+
+/**
+ * Cart Total page Component
+ * 
+ * @description
+ * displays the cart totals including sustotal,shipping fee and total cart amount.
+ * 
+ * This component pulls its data from the global 'shopContext'
+ * 
+ * @components
+ * @returns {JSX.Element} The cart totals UI
+ * 
+ * 
+*/
+
 const cartTotal = () => {
 
-    const {currency,delivery_fee,getCartAmount} = useContext(shopContext);
+  const { currency, delivery_fee, getCartAmount } = useContext(shopContext);
 
   return (
     <div className='w-full'>
       <div className='text-2xl'>
-         <Title text1={'CART'} text2={'TOTALS'}/>
+        <Title text1={'CART'} text2={'TOTALS'} />
       </div>
 
       <div className='flex flex-col gap-2 mt-2 text-sm'>
-           <div className='flex justify-between'>
-               <p>Subtotal</p>
-               <p>{currency} {getCartAmount()}.00</p>
-           </div>
-           <hr />
-           <div className='flex justify-between'>
-                <p>Shipping Fee</p>
-                <p>{currency} {delivery_fee}.00</p>
-           </div>
-           <hr />
-           <div className='flex justify-between'>
-              <b>Total</b>
-              <b>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00</b>
-           </div>
+        <div className='flex justify-between'>
+          <p>Subtotal</p>
+          <p>{currency} {getCartAmount()}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+          <p>Shipping Fee</p>
+          <p>{currency} {delivery_fee}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+          <b>Total</b>
+          <b>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00</b>
+        </div>
       </div>
     </div>
   )
