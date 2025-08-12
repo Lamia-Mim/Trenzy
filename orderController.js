bdb2309dae6ddac7acdb7be379366d79d831eae8
Modassir Hasan <92595088+Modassir-Hasan@users.noreply.github.com>
2025-08-12 15:56:46 +0600

    Add files via upload

---
+++ orderController.js
@@ -0,0 +1,33 @@
+
+
+// Placing orders using COD Method
+const placeOrder = async (req,res) => {
+
+}
+
+// Placing orders using bKash Method
+const placeOrderbKash = async (req,res) => {
+    
+}
+
+// Placing orders using nagad Method
+const placeOrdernagad = async (req,res) => {
+    
+}
+
+// All Orders data for Admin Panel
+const allOrders = async (req,res) => {
+
+}
+
+// User Order Data for Frontend
+const userOrders = async (req,res) => {
+    
+}
+
+// update order status from Admin Panel
+const updateStatus = async (req,res) => {
+    
+}
+
+export {placeOrder, placeOrderbKash, placeOrdernagad, allOrders, userOrders, updateStatus}
