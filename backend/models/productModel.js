36e38d5a6452f3f9e719dd0ca12605e8f2f85535
Lamia Islam Mim <lamiaislam2001@gmail.com>
2025-08-13 20:47:55 +0600

    Did Documentation

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
+/**
+ * Mongoose schema for a Product.
+ *
+ * @typedef {Object} Product
+ * @property {string} name - Name of the product 
+ * @property {string} description - Description of the product 
+ * @property {number} price - Price of the product 
+ * @property {Array<string>} image - Array of image URLs 
+ * @property {string} category - Main category of the product
+ * @property {string} subCategory - Sub-category of the product 
+ * @property {Array<string>} sizes - Available sizes 
+ * @property {boolean} [bestSeller] - Indicates if product is a best seller 
+ * @property {number} date - Time of product creation 
+ */
+
 const productSchema = new mongoose.Schema({
     
     name: { type: String, required: true },
